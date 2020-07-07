ffc41c7289d8fd55e2b3839cf9fc966f4e885d8a
Addy <zaristus@gmail.com>
2020-07-07 14:19:49 -0700

    more placeholder maps, standardized cart and product css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,44 +1,46 @@
-import React from 'react';
+import React from "react";
 import Modal from "./Modal";
-import { connect } from 'react-redux';
-import { openModal } from '../actions/modalActions';
+import { connect } from "react-redux";
+import { openModal } from "../actions/modalActions";
 import Navigation from "./Navigation";
 import CategoryBar from "./CategoryBar";
 
-
 const Cart = (props) => {
+  let cart = [
+    { name: "camera", price: 100 },
+    { name: "bag", price: 50 },
+  ];
 
-    return (
-        <>
-            <Modal />
-            <Navigation />
-            <CategoryBar />
-            <main className='cart__main'>
-                {/*Will map user's cart from state*/}
-                <div className='cart__item'>
-                    <div className='cart__item-img'>image</div>
-                    <div className='cart__item-nameprice-container'>
-                        <div className='cart__item-name'>Product Name</div>
-                        <div className='cart__item-price'>$100.00</div>
-                    </div>
-                </div>
-                <div className='cart__checkout'>
-                    <button onClick={() => props.openModal("checkout")}>Checkout</button>
-                </div>
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <Modal />
+      <Navigation />
+      <CategoryBar />
+      <main className="cart__main">
+        {cart.map((product, i) => {
+          return (
+            <div className="cart__item">
+              <div className="cart__item-img">image</div>
+              <div className="cart__item-nameprice-container">
+                <div className="cart__item-name">{product.name}</div>
+                <div className="cart__item-price">${product.price}</div>
+              </div>
+            </div>
+          );
+        })}
 
-const mapDispatchToProps = dispatch => {
-    return {
-        openModal: (modal) => dispatch(openModal(modal))
-    }
-}
+        <div className="cart__checkout">
+          <button onClick={() => props.openModal("checkout")}>Checkout</button>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(
-    Cart
-);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openModal: (modal) => dispatch(openModal(modal)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Cart);
