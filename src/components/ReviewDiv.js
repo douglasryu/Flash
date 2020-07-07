cef6dbb43580f1220fb19ec25ff496d50b828d90
aderyn1121 <zaristus@gmail.com>
2020-07-07 09:23:31 -0700

    product page style outline

--- src/components/ReviewDiv.js
+++ src/components/ReviewDiv.js
@@ -1,30 +1,30 @@
 import React from 'react'
 
-const ReviewDiv = (props) =>{
-        return(
-            <div>
-                {/* Todo - map this.state.reviews for productID into separate divs */}
-                <div className='profile__review-block'>
-                    <div className='profile__review-author'>Author</div>
-                    <div className='profile__review-content'>Review content</div>
-                    <button className='profile__review-button'>Delete/Edit (maybe)</button>
-                </div>
-
-                <form className='profile__review-form'>
-                    {/* onSubmit = this.postReview function */}
-                    <textarea className='profile__review-field' type='text' placeholder='Submit a review'></textarea>
-                    <button className='profile__review-submit'>Submit</button>
-                </form>
+const ReviewDiv = (props) => {
+    return (
+        <div className='products__review-container'>
+            {/* Todo - map this.state.reviews for productID into separate divs */}
+            <div className='products__review-block'>
+                <div className='products__review-author'>Author</div>
+                <div className='products__review-content'>Review content</div>
+                <button className='products__review-button'>Delete/Edit (maybe)</button>
             </div>
-        )
-    }
+
+            <form className='products__review-form'>
+                {/* onSubmit = this.postReview function */}
+                <textarea className='products__review-field' type='text' placeholder='Submit a review'></textarea>
+                <button className='products__review-submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
 
 
 
 //Todo - get data from backend, set to this.state.reviews
 
-   //todo Post review function
+//todo Post review function
 
-   //Todo, possible Edit/Delete functions
+//Todo, possible Edit/Delete functions
 
 export default ReviewDiv;
