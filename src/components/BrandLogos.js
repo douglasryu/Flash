466dc0cc1b82bcc593cc5fb8b9b9434e0e12a9a4
douglasryu <douglasryu@hotmail.com>
2020-07-07 13:42:49 -0400

    Update carousel

---
+++ src/components/BrandLogos.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BrandLogos = () => {
+    return (
+        <div className="brand__logos--outer">
+            <div className="brand__logos--container">
+                <div className="brand__logo--canon">Canon</div>
+                <div className="brand__logo--nikon">Nikon</div>
+                <div className="brand__logo--sony">Sony</div>
+                <div className="brand__logo--fujifilm">FujiFilm</div>
+                <div className="brand__logo--olympus">Olympus</div>
+                <div className="brand__logo--leica">Leica</div>
+            </div>
+        </div>
+    );
+}
+
+export default BrandLogos;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./stylesheets/reset.css";
 import "./stylesheets/index.css";
 import "./stylesheets/navigation.css";
 import "./stylesheets/modal.css";
+import "./stylesheets/main.css";
+import "./stylesheets/brandlogo.css";
 import App from "./App";
 
 const store = configureStore();
